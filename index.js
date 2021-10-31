f86f8623fbc2a2f455102f4aace8ec755768996d
Georgi Dochev <georgi.p.dochev@gmail.com>
2021-10-31 21:16:31 +0200

    final

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-const express = require('express')
-const asyncHandler = require('express-async-handler')
+const express = require("express");
+const cors = require("cors");
+const superagent = require('superagent');
 
-let app = express()
+let app = express();
+app.use(cors());
 
 const PUBLIC_PATH = `${__dirname}/public/`;
 
-app.use(express.urlencoded( {extended : false}));
+app.use(express.urlencoded({ extended: false }));
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.sendFile(PUBLIC_PATH + 'index.html');
-})
-
-// GET method route
-app.get('/', function (req, res) {
-    res.send('GET request to the homepage')
-})
-  
-// POST method route
-app.post('/weather', asyncHandler(async(req, res) => {
-    let cityName = await req.body.city_name;
-    console.log(cityName);
-    res.send('POST request to the weather page');
-}))
-
-app.get('/weather', function (req, res) {
-    res.sendFile(PUBLIC_PATH + 'weather.html');
-})
-
-app.listen(3000, function() {
-    console.log("Server running on port 3000");
+app.get("/", function (req, res) {
+  res.sendFile(PUBLIC_PATH + "index.html");
 });
+
+app.get("/", function (req, res) {
+  res.send("GET request to the homepage");
+});
+
+app.post("/weather", function (req, res) {
+  let cityName = req.query.city;
+  callWeather(cityName, res);
+});
+
+app.get("/weather", function (req, res) {
+  res.sendFile(PUBLIC_PATH + "weather.html");
+});
+
+app.listen(80, function () {
+  console.log("Server running on port 80");
+});
+
+function callWeather(cityName, clientResponse){
+  let response;
+  superagent.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=8f3c69e4c232321d91aa7d0ecbadbc5f&units=metric`)
+  .end((err, res) => {
+    if (err) { return console.log(err); }
+    response = res.body;
+    clientResponse.send(response);
+  });
+  return response;
+}
